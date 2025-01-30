f1d8b3bb5e4069d420d37d6e101574de4e0ba0bf
Chase kochis <chasekochis@MacBook-Pro-12.ad.pcc.edu>
2025-01-30 13:06:01 -0800

    did work

--- sketch.js
+++ sketch.js
@@ -2,7 +2,19 @@ function setup() {
   createCanvas(400, 400);
 }
 
+
+
+
+
 function draw() {
-  background(200);
+  background('white');
+
+  fill('DarkMagenta');
+  rect(100,100, 40, 40);
+  arc(50, 50, 80, 80, 0, PI + HALF_PI);
+
+line(25, 25, 500, 500);
+
+
 }
 // test test test
